4cf0eb42dc3e7d22413347581a1e799291e737eb
Stewart Lait <stewartlait@hotmail.co.uk>
2024-10-12 13:24:34 +0100

    Update functionality
    - Remove email sender, send direct to email
    - Add newlsetter check
    - Add axios, delete api/collect
    - Stop image downloading to browser, send direct to email
    - Improve Form Validation

--- src/App.jsx
+++ src/App.jsx
@@ -1,100 +1,104 @@
-import React, { useState } from 'react';
-import ConsentPopup from './popup';
-import CameraComponent from './CameraComponent';
-import Details from './details';
-import ThankYou from './ThankYou';  
-import './App.css';
+import React, { useState } from "react";
+import ConsentPopup from "./popup";
+import CameraComponent from "./CameraComponent";
+import Details from "./details";
+import ThankYou from "./ThankYou";
+import "./App.css";
 
 const App = () => {
-    const [hasAgreed, setHasAgreed] = useState(false);
-    const [capturedImage, setCapturedImage] = useState(null);
-    const [showDetails, setShowDetails] = useState(false);
-    const [showThankYou, setShowThankYou] = useState(false); 
+  const [hasAgreed, setHasAgreed] = useState(false);
+  const [capturedImage, setCapturedImage] = useState(null);
+  const [showDetails, setShowDetails] = useState(false);
+  const [showThankYou, setShowThankYou] = useState(false);
+  const [newsLetter, setNewsletter] = useState(false);
 
-    const handleAgreement = () => {
-        setHasAgreed(true);
-    };
+  const handleAgreement = (newsLetterConsert) => {
+    setHasAgreed(true);
+    setNewsletter(newsLetterConsert);
+  };
 
-    const handleImageCapture = (imageUrl) => {
-        setCapturedImage(imageUrl);
-    };
+  const handleImageCapture = (imageUrl) => {
+    setCapturedImage(imageUrl);
+  };
 
-    const handleContinue = () => {
-        if (capturedImage) {  // Ensure capturedImage is not null
-            console.log('Navigating to Details page');
-            setShowDetails(true);
-        } else {
-            console.error('No captured image available to continue.');
+  const handleContinue = () => {
+    if (capturedImage) {
+      // Ensure capturedImage is not null
+      console.log("Navigating to Details page");
+      setShowDetails(true);
+    } else {
+      console.error("No captured image available to continue.");
+    }
+  };
+
+  const handleBackToCamera = () => {
+    setCapturedImage(null);
+    setShowDetails(false);
+    console.log("its' triggering badck to camera");
+  };
+
+  const shareImage = async () => {
+    if (capturedImage) {
+      const blob = await fetch(capturedImage).then((res) => res.blob());
+      const file = new File([blob], "captured-image.png", {
+        type: "image/png",
+      });
+
+      if (navigator.share) {
+        try {
+          await navigator.share({
+            title: "Check out this image!",
+            text: `Here is the image I captured.`,
+            files: [file],
+            url: window.location.href,
+          });
+          console.log("Image shared successfully");
+          setShowThankYou(true);
+        } catch (error) {
+          console.error("Error sharing the image:", error);
+          setShowThankYou(true);
         }
-    };
+      } else {
+        setShowThankYou(true);
+      }
+    }
+  };
 
-    const handleBackToCamera = () => {
-        setCapturedImage(null);
-        setShowDetails(false);
-        console.log("its' triggering badck to camera");
-    };
+  const resetApp = () => {
+    setHasAgreed(false);
+    setCapturedImage(null);
+    setShowDetails(false);
+    setShowThankYou(false);
+    setNewsletter(false);
+  };
 
-    const shareImage = async (emailAddress) => {
-        if (capturedImage) {
-            const blob = await fetch(capturedImage).then(res => res.blob());
-            const file = new File([blob], 'captured-image.png', { type: 'image/png' });
-
-            if (navigator.share) {
-                try {
-                    await navigator.share({
-                        title: 'Check out this image!',
-                        text: `Here is the image I captured. Send it to: ${emailAddress}`,
-                        files: [file],
-                        url: window.location.href
-                    });
-                    console.log('Image shared successfully');
-                    setShowThankYou(true); 
-                } catch (error) {
-                    console.error('Error sharing the image:', error);
-                    openEmailClient(emailAddress);
-                    setShowThankYou(true); 
-                }
-            } else {
-                openEmailClient(emailAddress); 
-                setShowThankYou(true);  
-            }
-        }
-    };
-
-  
-    const resetApp = () => {
-        setHasAgreed(false);
-        setCapturedImage(null);
-        setShowDetails(false);
-        setShowThankYou(false); 
-    };
-
-    return (
-        <div>
-            {!hasAgreed ? (
-                <ConsentPopup onAgree={handleAgreement} />
-            ) : showThankYou ? (  
-                <ThankYou onReset={resetApp} />  
-            ) : (
-                <>
-                    {!showDetails ? (
-                        <CameraComponent 
-                            onImageCapture={handleImageCapture}
-                            capturedImage={capturedImage}
-                            onBackToCamera={handleBackToCamera}
-                            onContinue={handleContinue}
-                        />
-                    ) : (
-                        <Details 
-                            capturedImage={capturedImage} 
-                            onShare={shareImage} 
-                            onReset={resetApp} 
-                        />
-                    )}
-                </>
-            )}
-        </div>
-    );
+  return (
+    <div>
+      {!hasAgreed ? (
+        <ConsentPopup onAgree={handleAgreement} />
+      ) : showThankYou ? (
+        <ThankYou onReset={resetApp} />
+      ) : (
+        <>
+          {!showDetails ? (
+            <CameraComponent
+              onImageCapture={handleImageCapture}
+              capturedImage={capturedImage}
+              onBackToCamera={handleBackToCamera}
+              onContinue={handleContinue}
+            />
+          ) : (
+            <Details
+              capturedImage={capturedImage}
+              onShare={shareImage}
+              onReset={resetApp}
+              newsLetter={newsLetter}
+            />
+          )}
+        </>
+      )}
+    </div>
+  );
 };
 
 export default App;
--- src/details.jsx
+++ src/details.jsx
@@ -1,147 +1,189 @@
-import React, { useState } from 'react';
-import ThankYou from './ThankYou'; 
-import "./detail.css"
+import React, { useState } from "react";
+import ThankYou from "./ThankYou";
+import "./detail.css";
+import axios from "axios";
 
-const Details = ({ capturedImage, onShare, onReset }) => {
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [number, setNumber] = useState('');
-    const [showThankYou, setShowThankYou] = useState(false);  
+const Details = ({ capturedImage, onShare, onReset, newsLetter }) => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [number, setNumber] = useState("");
+  const [showThankYou, setShowThankYou] = useState(false);
+  const phoneNumberRegex = /^\+?[0-9]{10,15}$/;
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  const nameRegex = /^[a-zA-Z\u0370-\u03FF\u1F00-\u1FFF\s]+$/; // Support Greek
 
-    const phoneNumberRegex = /^[0-9]{10}$/;
-
-    const validateForm = () => {
-        if (!name.trim()) {
-            alert('Please enter your name.');
-            return false;
-        }
-        if (!email.trim()) {
-            alert('Please enter your email.');
-            return false;
-        }
-        if (!number.trim()) {
-            alert('Please enter your phone number.');
-            return false;
-        }
-        if (!phoneNumberRegex.test(number)) {
-            alert('Please enter a valid 10-digit phone number.');
-            return false;
-        }
-        return true;
-    };
-
-    const handleSubmit = async () => {
-        const userDetails = { name, email, number };
-
-        try {
-            const response = await fetch('/api/collect', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(userDetails),
-            });
-
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-
-            const data = await response.json();
-            console.log('Success:', data);
-            alert('Successfully registered!');
-            setName('');
-            setEmail('');
-            setNumber('');
-            setShowThankYou(true); 
-            return true;
-        } catch (error) {
-            console.error('Error:', error);
-            return false;
-        }
-    };
-
-    const handleShareClick = async () => {
-        const isFormValid = validateForm();
-
-        if (isFormValid) {
-            const isSubmitted = await handleSubmit();
-
-            if (isSubmitted && capturedImage) {
-                onShare();
-            } else if (!capturedImage) {
-                alert('No image available to share.');
-            }
-        }
-    };
-
-    if (showThankYou) {
-        return <ThankYou />;  
+  const validateForm = () => {
+    if (!name.trim()) {
+      alert("Please enter your name.");
+      return false;
+    } else if (!nameRegex.test(name)) {
+      alert("Name cannot contain digits or symbols.");
+      return false;
     }
+    if (!email.trim()) {
+      alert("Please enter your email.");
+      return false;
+    } else if (!emailRegex.test(email)) {
+      alert("Please enter a valid email address.");
+      return false;
+    }
+    if (!number.trim()) {
+      alert("Please enter your phone number.");
+      return false;
+    } else if (!phoneNumberRegex.test(number)) {
+      alert("Please enter a valid phone number.");
+      return false;
+    }
+    return true;
+  };
 
-    return (
-        <>
-        
-        <div className='form-header'>
-            <div className='form-header-inside'>
-            <h2 className='detail-header'>Συμμετοχή<br/>
-                        στην κλήρωση<br/>
-                        & αποστολή<br/>
-                        της φωτογραφίας</h2>
+  const handleSubmit = async () => {
+    const convertToBase64 = (blob) => {
+      return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(blob);
+        reader.onloadend = () => resolve(reader.result);
+        reader.onerror = (error) => reject(error);
+      });
+    };
 
-            <form onSubmit={(e) => e.preventDefault()}>
+    try {
+      const blob = await fetch(capturedImage).then((res) => res.blob());
+      const base64Image = await convertToBase64(blob);
+
+      // 1. Send user details to the Backend API
+      const response = await axios.post(
+        "https://vercel-serverless-func-seven.vercel.app/api/save-user",
+        {
+          name: name,
+          email: email,
+          phoneNumber: number,
+          hasNewsletter: newsLetter,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      console.log("User saved:", response.data);
+
+      // 2. Pass email and Base64 image string to the Backend API
+      const emailResponse = await axios.post(
+        "https://vercel-serverless-func-seven.vercel.app/api/send-email-image",
+        {
+          email: email,
+          image: base64Image,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      console.log("Email sent:", emailResponse.data);
+      alert("Successfully registered!");
+      setName("");
+      setEmail("");
+      setNumber("");
+      setShowThankYou(true);
+      return true;
+    } catch (error) {
+      console.error("Error:", error);
+      return false;
+    }
+  };
+
+  const handleShareClick = async () => {
+    const isFormValid = validateForm();
+    if (isFormValid) {
+      const isSubmitted = await handleSubmit();
+      if (isSubmitted && capturedImage) {
+        onShare();
+      } else if (!capturedImage) {
+        alert("No image available to share.");
+      }
+    }
+  };
+
+  if (showThankYou) {
+    return <ThankYou />;
+  }
+
+  return (
+    <>
+      <div className="form-header">
+        <div className="form-header-inside">
+          <h2 className="detail-header">
+            Συμμετοχή
+            <br />
+            στην κλήρωση
+            <br />
+            & αποστολή
+            <br />
+            της φωτογραφίας
+          </h2>
+
+          <form onSubmit={(e) => e.preventDefault()}>
+            <div>
+              <h6 className="sub-header">Πεδία υποχρεωτικής συμπλήρωσης:</h6>
+              <div className="form-label-input">
+                <label htmlFor="name">
+                  <b>Ονοματεπώνυμο</b>
+                </label>
+                <br />
+                <input
+                  type="text"
+                  id="name"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  required
+                />
+
                 <div>
-                    <h6 className='sub-header'>Πεδία υποχρεωτικής συμπλήρωσης:</h6>
-                    <div className='form-label-input'>
-                    <label htmlFor="name"><b>Ονοματεπώνυμο</b></label>
-                    <br/>
-                    <input
-                        type="text"
-                        id="name"
-                        value={name}
-                        onChange={(e) => setName(e.target.value)}
-                        required
-                    />
-               
-              
-                <div>
-                    <label htmlFor="number"><b>Τηλέφωνο</b></label>
-                    <br/>
-                    <input
-                        type="text"
-                        id="number"
-                        value={number}
-                        onChange={(e) => setNumber(e.target.value)}
-                        required
-                    />
+                  <label htmlFor="number">
+                    <b>Τηλέφωνο</b>
+                  </label>
+                  <br />
+                  <input
+                    type="text"
+                    id="number"
+                    value={number}
+                    onChange={(e) => setNumber(e.target.value)}
+                    required
+                  />
                 </div>
                 <div>
-                    <label htmlFor="email"><b>Email:</b></label>
-                    <br/>
-                    <input
-                        type="email"
-                        id="email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        required
-                    />
+                  <label htmlFor="email">
+                    <b>Email:</b>
+                  </label>
+                  <br />
+                  <input
+                    type="email"
+                    id="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                  />
                 </div>
-                </div>
-                </div>
-            </form>
-            
-            
+              </div>
+            </div>
+          </form>
         </div>
-        </div>
-        <div className="submit-button">
-        <img  src='/buttons/dheeraj.png' alt='submit' onClick={handleShareClick} 
-       style={{ 
-        
-        width: '100px'
-    }} />
-        </div>
-        
-        </>
-    );
+      </div>
+      <div className="submit-button">
+        <img
+          src="/buttons/dheeraj.png"
+          alt="submit"
+          onClick={handleShareClick}
+          style={{
+            width: "100px",
+          }}
+        />
+      </div>
+    </>
+  );
 };
 
 export default Details;
