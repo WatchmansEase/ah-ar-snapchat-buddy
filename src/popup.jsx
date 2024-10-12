102d7bd6a03b3437dd740dbca60470f99a26a685
Watchman <37624626+WatchmansEase@users.noreply.github.com>
2024-10-12 13:32:46 +0100

    Update functionality (#3)
    - Remove email sender, send direct to email
    - Add newlsetter check
    - Add axios, delete api/collect
    - Stop image downloading to browser, send direct to email
    - Improve Form Validation

--- src/popup.jsx
+++ src/popup.jsx
@@ -1,83 +1,73 @@
-import React, { useState } from 'react';
-import './popup.css';  // Import the popup.css file
+import React, { useState } from "react";
+import "./popup.css"; // Import the popup.css file
 
 const ConsentPopup = ({ onAgree }) => {
-    const [isChecked, setIsChecked] = useState(false);
-    const [isVisible, setIsVisible] = useState(true);
-    const [newsLetter,setNewsletter] = useState(false);
-    const handleCheckboxChange = () => {
-        setIsChecked(!isChecked);
-    };
-    const handleCheckboxChangeNewsletter = () => {
-        setNewsletter(!newsLetter);
-    };
+  const [isChecked, setIsChecked] = useState(false);
+  const [isVisible, setIsVisible] = useState(true);
+  const [newsLetter, setNewsletter] = useState(false);
+  const handleCheckboxChange = () => {
+    setIsChecked(!isChecked);
+  };
+  const handleCheckboxChangeNewsletter = () => {
+    setNewsletter(!newsLetter);
+  };
 
+  const handleSubmit = () => {
+    if (isChecked) {
+      onAgree();
+      setIsVisible(false);
+    } else {
+      alert("You must agree to the terms to proceed.");
+    }
+  };
 
-    const handleSubmit = () => {
-        if (isChecked) {
-            onAgree(); 
-            setIsVisible(false); 
-        } else {
-            alert("You must agree to the terms to proceed.");
-        }
-    };
-
-    return (
-        isVisible && (
-            <div className='popup-first'>
-            <div className="popup">
-                
-                <h2 className='pop-first-title'>Διαγωνισμός</h2>
-                    <h2 className='pop-second-title'>για να είσαι μέσα σε όλα!</h2>
-                    <h4>Λάβε μέρος!</h4>
-                    <div className='popup-img-flex'>
-                  <img src="/buttons/popupbuttonAsset 7.png" alt="Logo" className="popup-image" onClick={handleSubmit} />
-                </div>
-                <div className='popup-input-text'>
-                {/* <div className='popup-h4-input'>
-                <input 
-                        type="checkbox" 
-                        checked={isChecked} 
-                        onChange={handleCheckboxChange} 
-                    /><h4>
-                    
-                Αποδέχομαι τους <a href=''>όρους</a> <br/>
-                του διαγωνισμού.
-                
+  return (
+    isVisible && (
+      <div className="popup-first">
+        <div className="popup">
+          <h2 className="pop-first-title">Διαγωνισμός</h2>
+          <h2 className="pop-second-title">για να είσαι μέσα σε όλα!</h2>
+          <h4>Λάβε μέρος!</h4>
+          <div className="popup-img-flex">
+            <img
+              src="/buttons/popupbuttonAsset 7.png"
+              alt="Logo"
+              className="popup-image"
+              onClick={handleSubmit}
+            />
+          </div>
+          <div className="popup-input-text">
+            <div className="popup-h4-input-2">
+              <div className="popup-h4-input-2-2">
+                <input
+                  type="checkbox"
+                  checked={isChecked}
+                  onChange={handleCheckboxChange}
+                />
+                <h4>
+                  Αποδέχομαι τους <a href="">όρους</a> <br />
+                  του διαγωνισμού.
                 </h4>
-                </div> */}
-                <div className='popup-h4-input-2'>
-                  
-                <div className='popup-h4-input-2-2'>
-                <input 
-                        type="checkbox" 
-                        checked={isChecked} 
-                        onChange={handleCheckboxChange} 
-                    />
-                <h4>                
-                Αποδέχομαι τους <a href=''>όρους</a> <br/>
-                του διαγωνισμού.
-          </h4>
-          </div>
-          <div className='popup-h4-input-2-2'>
-                <input 
-                        type="checkbox" 
-                        checked={newsLetter} 
-                        onChange={handleCheckboxChangeNewsletter} 
-                    />
-                <h4>                
-                   Επιθυμώ να λαμβάνω ενημερώσεις<br/>
-                σχετικά με προϊόντα και
-                νέες<br/> υπηρεσίες του Ομίλου ΔΕΗ.
-          </h4>
-          </div>
-             </div>
-                </div>
-           
+              </div>
+              <div className="popup-h4-input-2-2">
+                <input
+                  type="checkbox"
+                  checked={newsLetter}
+                  onChange={handleCheckboxChangeNewsletter}
+                />
+                <h4>
+                  Επιθυμώ να λαμβάνω ενημερώσεις
+                  <br />
+                  σχετικά με προϊόντα και νέες
+                  <br /> υπηρεσίες του Ομίλου ΔΕΗ.
+                </h4>
+              </div>
             </div>
-            </div>
-        )
-    );
+          </div>
+        </div>
+      </div>
+    )
+  );
 };
 
 export default ConsentPopup;
