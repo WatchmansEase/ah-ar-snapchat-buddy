7f65929ebac01c1e351ec90c6aaa96e3b570e458
Stewart Lait <stewartlait@hotmail.co.uk>
2024-10-14 04:50:31 +0100

    Resize all buttons

--- src/CaptureCamera.jsx
+++ src/CaptureCamera.jsx
@@ -1,19 +1,21 @@
-import React from 'react';
-import "./LiveCamera.css"
-const CaptureControls = ({ onCapture}) => {
-    return (
-        <div className='capture-button-container'>
-            <img 
-                src="/buttons/fcameraAsset 12@2x.png"  
-                alt="Capture"
-                className="capture-button"
-                onClick={onCapture}
-                style={{ 
-                    cursor: 'pointer',width:'40px',height:'40px'
-                }} 
-            />
-        </div>
-    );
+import React from "react";
+import "./LiveCamera.css";
+const CaptureControls = ({ onCapture }) => {
+  return (
+    <div className="capture-button-container">
+      <img
+        src="/buttons/fcameraAsset 12@2x.png"
+        alt="Capture"
+        className="capture-button"
+        onClick={onCapture}
+        style={{
+          cursor: "pointer",
+          width: "100px",
+          height: "100px",
+        }}
+      />
+    </div>
+  );
 };
 
 export default CaptureControls;
--- src/ResetButton.jsx
+++ src/ResetButton.jsx
@@ -3,14 +3,12 @@ const ResetButton = ({ onReset }) => {
   return (
     <div className="thankyou-container">
       <h1>Thank you!</h1>
-      <div className="restart-name">
-        <img
-          src="/buttons/frestartAsset 15@3x.png"
-          onClick={onReset}
-          alt="restart"
-          className="restart-button"
-        ></img>
-      </div>
+      <img
+        src="/buttons/frestartAsset 15@3x.png"
+        onClick={onReset}
+        alt="restart"
+        className="restart-button"
+      ></img>
     </div>
   );
 };
