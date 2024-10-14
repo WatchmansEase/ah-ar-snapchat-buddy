4df9e92dade75bfdd6278d5601b14aedcc495245
Manish <man98singh@gmail.com>
2024-10-14 12:22:49 +0530

    optimized ar- peformance

--- src/ImagePreview.jsx
+++ src/ImagePreview.jsx
@@ -1,63 +1,42 @@
-const ImagePreview = ({ capturedImage, onBack, onContinue }) => {
-  const handleBack = async () => {
-    try {
-      await onBack();
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  const handleContinue = async () => {
-    try {
-      await onContinue();
-    } catch (error) {
-      console.error(error);
+import React, { useState } from "react";
+import "./ImagePreview.css";
+const ImagePreview = ({ capturedImage, onBack, onContinue, onShare }) => {
+  const handleShare = () => {
+    if (capturedImage) {
+      onShare();
+    } else {
+      alert("No image available to share.");
     }
   };
 
   return (
     <>
+      {" "}
       <div className="image-container">
-        <img
-          className="image-preview"
-          src={capturedImage}
-          alt="Captured"
-          loading="lazy"
-        />
+        <img className="image-preview" src={capturedImage} alt="Captured" />
         <div className="poweredby">
           <div
             className="mukul"
-            style={{
-              position: "absolute",
-              bottom: "40px",
-              left: "50%",
-              transform: "translateX(-50%)",
-              zIndex: 4,
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-              gap: "10px",
-              width: "80%",
-            }}
+          
           >
             <div className="button-container">
               <img
                 src="\buttons\fretryAsset 13.png"
                 alt="retry"
                 className="back-button"
-                onClick={handleBack}
+                onClick={onBack}
               />
               <img
                 src="\buttons\fcontinueAsset 14.png"
                 alt="Continue"
                 className="continue-button"
-                onClick={handleContinue}
+                onClick={onContinue}
               />
             </div>
           </div>
           <img
             src="\buttons\Dark BG.png"
-            alt="Overlay Image"
+            alt="Overlaytr Image"
             className="poweredby-image"
           />
         </div>
